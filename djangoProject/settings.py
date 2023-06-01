e292b22aa063e48ada95fceba947efa752bc544d
kristop2707 <95447559+kristop2707@users.noreply.github.com>
2023-06-01 17:40:37 +0200

    copying files from master-recovery

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -43,9 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'timeline',
-     'django_extensions',
-
+    'timeline'
 ]
 
 MIDDLEWARE = [
@@ -129,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "timeline/static"),
+   os.path.join(BASE_DIR, "static"),
 ]
 
 MEDIA_URL = '/media/'
